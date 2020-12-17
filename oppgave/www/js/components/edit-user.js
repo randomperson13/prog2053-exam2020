31265b56c8a32a17c5b1d0f4ccfaf05b095636d2
knutmagk <knutmagk@stud.ntnu.no>
2020-12-17 12:20:17 +0100

    updated edit-user

--- oppgave/www/js/components/edit-user.js
+++ oppgave/www/js/components/edit-user.js
@@ -8,6 +8,34 @@ class EditUser extends LitElement {
   }
 
   // din kode her
+  render() {
+    return html`
+      <form onsubmit="javascript: return false;" id="userForm" method="post">
+      <div class="form-group">
+      <label for="email">Email</label>
+      <input  class="form-control" id="firstName" name="firstName" type="text" value="${this.user.firstName}" required>
+      </div>
+      <input type="submit" @click=${this.updateUser} id="submitForm" name="editUser" class="btn" value="Edit User"></input>
+      </form>
+    `;
+}
+
+  updateUser(e){
+    const dataForm = new FormData(e.target.form);
+    fetch('api/updateUser.php', {
+      method: 'POST',
+      body: dataForm
+    }).then(res=>res.json())
+    .then(data=>{
+      if (data.status=='success'){
+        console.log("The user was updated");
+      } else {
+        console.log("The user was not updated");
+      }
+    })
+  }
+
+
 
 }
 customElements.define('edit-user', EditUser);
